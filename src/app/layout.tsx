2cd78508d7de0f5b809e8eb3490d1cac7a6c8f44
Jeremy Santoso <97815661+jeromedesantos12@users.noreply.github.com>
2025-10-19 01:26:40 +0700

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import type { ReactNode } from "react";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/hooks/theme-provider";
 import "./globals.css";
 
 const geistSans = Geist({
